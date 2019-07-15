71561967150f04f4b94644d92f9df9d40de738fd
rayyyy <nraavy@gmail.com>
2019-07-15 13:31:45 +0900

    model ad

---
+++ models/user.go
@@ -0,0 +1,20 @@
+package models
+
+import "time"
+
+type User struct {
+	Birthday  time.Time `gorm:"column:birthday"`
+	CreatedAt time.Time `gorm:"column:created_at"`
+	Email     string    `gorm:"column:email"`
+	ID        int       `gorm:"column:id;primary_key"`
+	Image     string    `gorm:"column:image"`
+	Nickname  string    `gorm:"column:nickname"`
+	Sex       string    `gorm:"column:sex"`
+	Status    string    `gorm:"column:status"`
+	UpdatedAt time.Time `gorm:"column:updated_at"`
+}
+
+// TableName sets the insert table name for this struct type
+func (u *User) TableName() string {
+	return "users"
+}
