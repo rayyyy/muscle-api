d9f36e9df0355231c5706200becc933318ee3ab6
rayyyy <nraavy@gmail.com>
2019-08-01 00:24:31 +0900

    fix

--- models/db.go
+++ models/db.go
@@ -12,8 +12,8 @@ func GetDB() (*gorm.DB, error) {
 	user := funcs.GetEnv("DATABASE_USER", "root")
 	pass := funcs.GetEnv("DATABASE_PASS", "pass")
 	host := funcs.GetEnv("DATABASE_HOST", "mysql")
-	database := funcs.GetEnv("DATABASE_NAME", "muscle_development?")
-	url := user + ":" + pass + "@tcp(" + host + ":3306)/" + database + "charset=utf8&parseTime=True&loc=Local"
+	database := funcs.GetEnv("DATABASE_NAME", "muscle_development")
+	url := user + ":" + pass + "@tcp(" + host + ":3306)/" + database + "?charset=utf8&parseTime=True&loc=Local"
 	db, err := gorm.Open("mysql", url)
 	return db, err
 }
