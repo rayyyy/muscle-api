1e96c2aa3ad045a401d9fc7e81c683d8778e5bb8
rayyyy <nraavy@gmail.com>
2019-08-19 18:12:11 +0900

    plantable

--- migration/20190819175017_create_mentor.sql
+++ migration/20190819175017_create_mentor.sql
@@ -1,13 +1,14 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
-CREATE TABLE mentor_plans (
+CREATE TABLE mentors (
   id INT(11) AUTO_INCREMENT NOT NULL,
-  mentor_id INT(11) NOT NULL,
-  type ENUM('subscription', 'once') DEFAULT 'once' NOT NULL,
+  user_id INT(11) NOT NULL,
   title VARCHAR(255) NOT NULL,
-  detail TEXT NOT NULL NOT NULL,
-  price INT NOT NULL,
-  is_valid BOOLEAN NOT NULL,
+  appeal_message TEXT NOT NULL,
+  image1 TEXT,
+  image2 TEXT,
+  image3 TEXT,
+  image4 TEXT,
   created_at datetime NOT NULL default current_timestamp,
   updated_at datetime NOT NULL default current_timestamp on update current_timestamp,
   deleted_at DATETIME,
@@ -17,4 +18,4 @@ CREATE TABLE mentor_plans (
 
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
-DROP TABLE mentor_plans;
+DROP TABLE mentors;
